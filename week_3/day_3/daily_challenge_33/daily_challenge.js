3e6c0f5c795bc308c50498d84d0117a257c10a5b
sixtinemd-dot <sixtinemd@gmail.com>
2025-11-26 09:40:36 +0200

    changed day 2 week 3

---
+++ week_3/day_3/daily_challenge_33/daily_challenge.js
@@ -0,0 +1,14 @@
+// Daily challenge: Tell the story
+
+// Instructions
+// In todays exercise we will be creating a Mad Libs game.
+// If you’ve never played this game, a mad lib is a game where you fill in a bunch of blank inputs with different word types (ie : noun, adjective, verb), and then a story is generated based on the words you chose, and sometimes the story is surprisingly funny.
+
+// Follow these steps :
+
+// Get the value of each of the inputs in the HTML file when the form is submitted. Remember the event.preventDefault()
+// Make sure the values are not empty
+// Write a story that uses each of the values.
+// Make sure you check the console for errors when playing the game.
+
+const 
