f5e1b0f0dcee6756fdd48095157769fbd2d96909
sixtinemd-dot <sixtinemd@gmail.com>
2026-01-08 18:04:28 +0100

    Remove exercises folder from repo

--- week_9/day_5/daily_challenge_95/daily-challenge/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import { plannerReducer } from "./reducer";
-
-export const store = createStore(
-  plannerReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
-);
--- week_9/day_5/exercices_XP_95/todo-app/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-// redux/store.js
-import { createStore } from "redux";
-import { todoReducer } from "./reducer";
-
-export const store = createStore(
-  todoReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
-);
