59e166bdc6dfa47a58b3ea24ed37888a79f796ba
sixtinemd-dot <sixtinemd@gmail.com>
2026-01-15 12:43:31 +0100

    removed day 5

--- week_10/day_5/recipe-book-project-105/src/model/RecipeItem.ts
+++
@@ -1,9 +0,0 @@
-export default class RecipeItem {
-  constructor(
-    public id: string,
-    public title: string,
-    public ingredients: string[],
-    public instructions: string,
-    public isFavorite: boolean = false
-  ) {}
-}
